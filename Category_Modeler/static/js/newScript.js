e6cd109ed0f2acb30beda52f9ae3ec22d5f14f88
prashant <prashantgupta.nz@gmail.com>
2014-10-07 18:36:01 +1300

    Ajax issue fixed

--- Category_Modeler/static/js/newScript.js
+++ Category_Modeler/static/js/newScript.js
@@ -1,100 +1,163 @@
-/**
- * @author Prashant Gupta
- */
-$(document).ready(function() {
+$(document)
+		.ready(
+				function() {
+					
 
-	$('.dropdown-toggle').dropdown();
+					function getCookie(name) {
+						var cookieValue = null;
+						if (document.cookie
+								&& document.cookie != '') {
+							var cookies = document.cookie
+									.split(';');
+							for (var i = 0; i < cookies.length; i++) {
+								var cookie = jQuery
+										.trim(cookies[i]);
+								// Does this cookie string
+								// begin with the name we
+								// want?
+								if (cookie.substring(0,
+										name.length + 1) == (name + '=')) {
+									cookieValue = decodeURIComponent(cookie
+											.substring(name.length + 1));
+									break;
+								}
+							}
+						}
+						return cookieValue;
+					}
 
-	$('input[name="testoption"]').click(function() {
-		if ($('input[name="testoption"]:checked').attr("id")=="trainingfile") 
-			{
-				$("#testfile").prop('disabled', false);
-				$("#Fold").prop('disabled', true);
-				$("#Percentage").prop('disabled', true);
-			} 
-		else if($('input[name="testoption"]:checked').attr("id")=="CrossValidation") 
-			{
-				$("#testfile").prop('disabled', true);
-				$("#Fold").prop('disabled', false);
-				$("#Percentage").prop('disabled', true);
-			
-			}
-		else if	($('input[name="testoption"]:checked').attr("id")=="PercentageSplit") 
-			{
-				$("#testfile").prop('disabled', true);
-				$("#Fold").prop('disabled', true);
-				$("#Percentage").prop('disabled', false);
-			
-			}
-			else{
-				$("#testfile").prop('disabled', true);
-				$("#Fold").prop('disabled', true);
-				$("#Percentage").prop('disabled', true);
-			}
-			;
-		
-	});
+					var csrftoken = getCookie('csrftoken');
 
+					function csrfSafeMethod(method) {
+						// these HTTP methods do not require
+						// CSRF protection
+						return (/^(GET|HEAD|OPTIONS|TRACE)$/
+								.test(method));
+					}
 
-$('#filename').change(function(){
-	var file = this.files[0];
-	var reader = new FileReader();
-    reader.readAsText(file);
-     reader.onload = function(event){
-      var csv = event.target.result;
-      var data = $.csv.toArrays(csv);
-     var $container = $('#dataTable');
-      $container.handsontable({
-    	  data: data
-      });
- /*         var html = '';
-          var instances = 0
-          var attributes = 0
-         html +='<tr>\r\n';
-      for (var head in data[0]){
-    	  html += '<th>' + data[0][head] + '</th>\r\n';
-    	  attributes +=1;
-      }
-      html += '</tr>\r\n';
-      
-      for(var row =1; row < data.length; row++ ) {
-        html += '<tr>\r\n';
-        instances +=1;
-        for(var item in data[row]) {
-          html += '<td>' + data[row][item] + '</td>\r\n';
-        }
-        html += '</tr>\r\n';
-      }
-      $('#contents').html(html);
-      $('#instances').html( instances);
-      $('#Attributes').html( attributes);*/
-    }
+					
+					$
+							.ajaxSetup({
+								beforeSend : function(xhr,
+										settings) {
+									if (!csrfSafeMethod(settings.type)
+											&& !this.crossDomain) {
+										xhr
+												.setRequestHeader(
+														"X-CSRFToken",
+														csrftoken);
+									}
+								}
+							});
 
+					$('.dropdown-toggle').dropdown();
 
+					$('input[name="testoption"]')
+							.click(
+									function() {
+										if ($(
+												'input[name="testoption"]:checked')
+												.attr("id") == "trainingfile") {
+											$("#testfile").prop('disabled',
+													false);
+											$("#Fold").prop('disabled', true);
+											$("#Percentage").prop('disabled',
+													true);
+										} else if ($(
+												'input[name="testoption"]:checked')
+												.attr("id") == "CrossValidation") {
+											$("#testfile").prop('disabled',
+													true);
+											$("#Fold").prop('disabled', false);
+											$("#Percentage").prop('disabled',
+													true);
 
+										} else if ($(
+												'input[name="testoption"]:checked')
+												.attr("id") == "PercentageSplit") {
+											$("#testfile").prop('disabled',
+													true);
+											$("#Fold").prop('disabled', true);
+											$("#Percentage").prop('disabled',
+													false);
+
+										} else {
+											$("#testfile").prop('disabled',
+													true);
+											$("#Fold").prop('disabled', true);
+											$("#Percentage").prop('disabled',
+													true);
+										}
+										;
+
+									});
+
+					//Function to set the height of training data table based on window size
+					function setHeight() {
+						var headerHeight = $('.container').outerHeight();
+						console.log(headerHeight);
+						var totalHeight = $(window).height();
+						console.log(totalHeight);
+						$('#dataTable').css({
+							'height' : totalHeight - headerHeight - 10 + 'px'
+						});
+					}
+
+					//call the setHeight function, everytime window is resized
+					$(window).on('resize', function() {
+						setHeight();
+					});
+					//call it for the first time
+					setHeight();
+
+					//Script to deal with when the training file is uploaded 
+					$('#trainingfile').change(function() {
+									var file = this.files[0];
+								//	console.log(file);
+									var reader = new FileReader(); //FileReader Object reads the content of file as text string into memory
+									reader.readAsText(file);
+									reader.onload = function(event) {
+										var csv = event.target.result;
+										var data = $.csv.toArrays(csv);
+										//console.log(data);
+										var instances = 0
+										var attributes = 0
+										for ( var head in data[0]) {
+											attributes += 1;
+										}
+										for (var row = 1; row < data.length; row++) {
+											instances += 1;
+										}
+										$('#instances').html(instances);
+										$('#Attributes').html(attributes);
 	
-/*	$.ajax({
-		type: "POST",
-		url: "http://127.0.0.1:8000/CategoryModeler/preprocess/",
-		dataType: "json",
-		async: true,
-		data: {
-			csrfmiddlewaretoken: '{{ csrf_token }}',
-			Filename: file
-			},
-		success: function(response) {
-			alert(response);
-		//	$('#tablename').text(json.filename);
-			
-		}
-	    
-				
-	});*/
-	
-});
+										var $container = $('#dataTable');
+										$container.handsontable({				//HandsonTable library to display interactive data table
+											data : data
+										});
+									};
+
+						
 
 
-	
+				//Posting file to the server side using formdata						
+										var formdata = new FormData();
+										formdata.append("trainingfile", file);
+										$.ajax({
 
+													type : "POST",
+													url : "http://127.0.0.1:8000/CategoryModeler/preprocess/",
+													dataType : "json",
+													async : true,
+													processData : false, // Don't process the files
+													contentType : false,
+													data : "dsbfjdsf",
+													success : function(response) {
+														alert(response);
 
-});
+													}
+												});
+
+									});
+
+				});
--- Category_Modeler/views.py
+++ Category_Modeler/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 import csv, json
-
+from io import FileIO, BufferedWriter
 
 
 # Create your views here.
@@ -11,21 +11,26 @@ def index(request):
 # The method allow user to upload the training data file, which is then saved on the server and displayed in a tabular format on the page
 def preprocess(request):
     if request.method == 'POST':
+        if request.is_ajax():
+            file = request.FILES['trainingfile']
+            print(file)
+            handle_uploaded_file(request, file);
+            
        # handle_uploaded_file(request.FILES['file'])
-        handle_uploaded_file(request.POST.get('FileName'))
-        name = request.POST.get('FileName')
+   #     handle_uploaded_file(request.POST.get('FileName'))
+   #     name = request.POST.get('FileName')
        # name = request.FILES['file'].name
-        print(name)
-        datafile = read_CSVFile(name)
-        attributes = datafile[0]
-        datafile.pop(0)
+   #     print(name)
+   #     datafile = read_CSVFile(name)
+   #     attributes = datafile[0]
+   #     datafile.pop(0)
         
-        request.session['current_trainingfile'] = name          #storing the current training data in the current session
-        response_data = {}
-        response_data['filename'] = name
-        response_data['csvfile'] = datafile
-        response_data['attributes'] = attributes
-        #return render(request, 'preprocess.html', {'filename':name , 'csvfile': datafile, 'attributes':attributes})
+   #     request.session['current_trainingfile'] = name  # storing the current training data in the current session
+   #     response_data = {}
+   #     response_data['filename'] = name
+   #     response_data['csvfile'] = datafile
+   #     response_data['attributes'] = attributes
+        # return render(request, 'preprocess.html', {'filename':name , 'csvfile': datafile, 'attributes':attributes})
     #    return HttpResponse(json.dumps(response_data), content_type="application/json")
         return HttpResponse("it's gud")
     else:
@@ -33,10 +38,16 @@ def preprocess(request):
 
 
 # create a file with similar name as provided in the static folder and copy all the contents    
-def handle_uploaded_file(f):
-    with open('Category_Modeler/static/uploaded_files/%s' % f, 'wb+') as dest:
-        for chunk in f.chunks():
-            dest.write(chunk)
+def handle_uploaded_file(request, f):
+    #with open('Category_Modeler/static/uploaded_files/%s' % f, 'wb+') as dest:
+    with BufferedWriter( FileIO( 'Category_Modeler/static/js/%s' % f, "wb" ) ) as dest:
+        foo = f.read(1024)
+        print(foo)
+        while foo:
+            dest.write(foo)
+            foo = f.read(1024)
+ #       for chunk in f.chunks():
+  #          dest.write(chunk)
         dest.close();
 
 #
