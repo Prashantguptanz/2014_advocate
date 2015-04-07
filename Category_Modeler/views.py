1469125dd4443507ef163bbfea32c7b3cd747c26
prashant <prashantgupta.nz@gmail.com>
2015-04-07 17:28:11 +1200

    updated handle_raster_file method

--- Category_Modeler/views.py
+++ Category_Modeler/views.py
@@ -20,54 +20,10 @@ def trainingsampleprocessing(request):
                 request.session['current_training_file'] = trainingfile.name
                 
                 if trainingfile.name.split(".")[-1] == ".csv":
-                    handle_uploaded_file(request, trainingfile);
+                    handle_uploaded_file(request, trainingfile)
                 else:
-                    dataset = gdal.Open('Category_Modeler/static/data/%s' % trainingfile.name)
-                    cols = dataset.RasterXSize
-                    rows = dataset.RasterYSize
-                    print cols, rows
-                    noOfBands = dataset.RasterCount
-                    bands = {}
-                    a=0
-                    final_array = {}
-                    print "I am at first place"
-                    for i in range(1, noOfBands):
-                        bands[i] = dataset.GetRasterBand(i).ReadAsArray(0,0,cols,rows)
-                    for j in range(rows):
-                        for k in range (cols):
-                            pixelValue = `j` + " " + `k` + " "
-                            for l in range(1, noOfBands):
-                                final_array[a] = pixelValue + `bands[l][j][k]` + " "
-                                a= a+1
-                    print final_array
-                    print "I am at second place"
+                    handle_raster_file(request, trainingfile)
                     
-            #        with BufferedWriter( FileIO( 'Category_Modeler/static/js/newfile.csv', "wb" ) ) as dest:
-            #            dest.write("x y band1 band2 band3 band4 band5 band6 band7 band8")
-            #            for i in range(100):
-            #                #print (final_array[i])
-            #                dest.write(final_array[i])
-            #            dest.close();
-            #            print "i am done"
-                    
-                     
-                
-            #test code to check if GDAL can be used to read raster data    
-            #dataset = gdal.Open('Category_Modeler/static/com74085cc1/hdr.adf')
-            #dataset = gdal.Open('Category_Modeler/static/data/akl.tif')
-            #cols = dataset.RasterXSize
-            #rows = dataset.RasterYSize
-            #print cols, rows
-            #count = dataset.RasterCount
-            #print count
-            #band = dataset.GetRasterBand(2)
-            #print band
-            #bandtype = gdal.GetDataTypeName(band.DataType)
-            #print bandtype
-            #scanline = band.ReadRaster(0, 0, band.XSize, 1, band.XSize, 1, band.DataType)
-            #print scanline
-            #data = band.ReadAsArray(0,0,cols,rows)
-            #print data
         return HttpResponse("")
     else:
         return render(request, 'trainingsample.html')
@@ -104,21 +60,37 @@ def handle_uploaded_file(request, f):
         dest.close();
 
 def handle_raster_file(request, f):
-    dataset = gdal.Open(f)
+    dataset = gdal.Open('Category_Modeler/static/data/%s' % f.name)
     cols = dataset.RasterXSize
     rows = dataset.RasterYSize
+    print cols, rows
     noOfBands = dataset.RasterCount
-    bands = {}
+    print noOfBands
+    bands = []
     
+    final_array = []
+    print "I am at first place"
     for i in range(1, noOfBands):
-        bands[i] = dataset.GetRasterBand(i).ReadAsArray(0,0,cols,rows)
+        bands.append(dataset.GetRasterBand(i).ReadAsArray(0,0,cols,rows))
+    print bands[2][1][2]
     for j in range(rows):
         for k in range (cols):
-            pixelValue = `j` + " " + `k` + ""
-            for l in range(1, noOfBands):
+            pixelValue = `j` + " " + `k` + " "
+            for l in range(len(bands)):
                 pixelValue = pixelValue + `bands[l][j][k]` + " "
-            print pixelValue
+            final_array.append(pixelValue)
+            
+    print final_array[0]
+    print "I am at second place"
     
+    with BufferedWriter( FileIO( 'Category_Modeler/static/js/newfile.csv', "wb" ) ) as dest:
+        dest.write("x y band1 band2 band3 band4 band5 band6 band7\n")
+        for i in range(len(final_array)):
+            #print (final_array[i])
+            dest.write(final_array[i] + '\n')
+        dest.close();
+        print "i am done"
+        
 #
 def read_CSVFile(f):
     with open('Category_Modeler/static/uploaded_files/%s' % f, 'rU') as datafile:
